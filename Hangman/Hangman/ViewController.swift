c6e1133d3daae1818bf964c629fb33358b675203
AaronCab <Aaron@pursuits-macbook-pro-17.local>
2018-11-26 20:44:03 -0500

    "Final Version"

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -8,87 +8,120 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-     var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    var guessCount = 6
-    var userCorrectLetters = Array(repeating: "_", count: activeWord.count)
-    var placeHolder = String()
-    var storedUserInput = [String]()
-   
-    
-    @IBOutlet weak var cohortInputTextField: UITextField!
-     @IBOutlet weak var userInputTextField: UITextField!
 
+class ViewController: UIViewController {
+    
+    var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var guessCount = 6
+    var storedUserInput = [String]()
+    var placeHolder = String()
+    var userCorrectLetters: [String] = []
+    @IBOutlet weak var cohortInputTextField: UITextField!
+    @IBOutlet weak var userInputTextField: UITextField!
+    
     @IBOutlet weak var guessLabel: UILabel!
     
     @IBOutlet weak var wordLabel: UILabel!
     
     @IBOutlet weak var hangManImage: UIImageView!
-    
-//    var placeHolder = String(repeating: "_", count: userInputTextField.count)
     override func viewDidLoad() {
-    super.viewDidLoad()
-    userInputTextField.delegate = self
-    cohortInputTextField.delegate = self
-    
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+        super.viewDidLoad()
+        userInputTextField.delegate = self
+        cohortInputTextField.delegate = self
+        
+    }
 }
 
+
 extension ViewController: UITextFieldDelegate {
-   
+    
+    fileprivate func imageSwitch() {
+        switch guessCount {
+        case 6:
+            hangManImage.image = UIImage(named: "hang1")
+        case 5:
+            hangManImage.image = UIImage(named: "hang2")
+        case 4:
+            hangManImage.image = UIImage(named: "hang3")
+        case 3:
+            hangManImage.image = UIImage(named: "hang4")
+        case 2:
+            hangManImage.image = UIImage(named: "hang5")
+        case 1:
+            hangManImage.image = UIImage(named: "hang6")
+        case 0:
+            hangManImage.image = UIImage(named: "hang7")
+        default:
+            print("nothing")
+        }
+    }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        while guessCount > 0 {
-            guessLabel.text = "You have 6 guesses left"
-            for letter in userInputTextField.text ?? ""{
-                if userInputTextField.contains(String(letter) as! UIFocusEnvironment){
-                    wordLabel.text = "\(letter, terminator: " ")"
-                } else {
-                    wordLabel.text = "\("_", terminator: " ")"
-                }
-            }
-            if let cohortInputTextField.text ?? "" {
-                print(" You played the letter \(cohortInputTextField.text ?? "")")
-                
-                guard alphabet.contains(cohortInputTextField.text ?? "")  else {
+        
+        imageSwitch()
+        if cohortInputTextField.text != "" {
+        while guessCount > 0{
+            if let userInput = cohortInputTextField.text?.uppercased(){
+                guard alphabet.contains(userInput)  else {
                     guessLabel.text = "That's not a valid letter! Please choose a letter."
                     continue
                 }
                 
-                guard !storedUserInput.contains(cohortInputTextField.text ?? "") else {
+                guard !storedUserInput.contains(userInput) else {
                     guessLabel.text = "Uh oh. Looks like you used this letter already. Try again!"
                     continue
                 }
-                storedUserInput.append(cohortInputTextField.text ?? "")
                 
-                if userInputTextField.contains(cohortInputTextField?.text as! UIFocusEnvironment ){
-                    for (index, char) in userInputTextField.enumerated() {
-                        if String(char) == cohortInputTextField.text ?? "" {
-                            userCorrectLetters[index] = cohortInputTextField.text ?? ""
+                storedUserInput.append(userInput)
+                
+                if userInputTextField.text!.contains(userInput){
+                    for (index, char) in userInputTextField.text!.enumerated() {
+                        if String(char) == userInput {
+                            userCorrectLetters[index] = userInput
+                            wordLabel.text = userCorrectLetters.joined(separator: " ")
                         }
                     }
                     
                     guessLabel.text = "Correct Choice, you still have \(guessCount) chances left!"
                 } else {
+                    
                     guessCount -= 1
+                    imageSwitch()
                     guessLabel.text = "Incorrect selection! You have \(guessCount) chances left!"
                 }
-            } else {
-                guessLabel.text = "That's not a valid Alphabet Character"
             }
-        if userInputTextField.text == cohortInputTextField.text {
-            print("It's a match")
+            if !userCorrectLetters.contains("_") {
+                wordLabel.text = "You Win"
+                
+            }
+            if guessCount == 0  {
+                wordLabel.text = "You Lose"
+            }
+            return true
         }
+        }
+        if userInputTextField.text != nil {
+            userCorrectLetters = Array(repeating: "_", count: Int(userInputTextField.text!.count))
+            guessLabel.text = "You have \(guessCount) guesses left"
+            for letter in userInputTextField.text! {
+                if storedUserInput.contains(String(letter))/* as? UIFocusEnvironment) */{
+                    wordLabel.text = "\(letter, terminator: " ")"
+                } else {
+                    wordLabel.text = userCorrectLetters.joined(separator: " ")
+                }
+                userInputTextField.text = userInputTextField.text?.uppercased()
+                return true
+            }
+            
+
+            
+        }
+        return true
+    }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         return true
-        }
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
-        return true
     }
-    }
-
+    
 }
-
---
+++ Hangman/Hangman/hangManBrain.swift
@@ -0,0 +1,16 @@
+//
+//  hangManBrain.swift
+//  Hangman
+//
+//  Created by Aaron Cabreja on 11/26/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+
+
+class HangManBrain {
+    
+}
+
